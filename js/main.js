0c0c73fed0025503a47853f63f43ea6e805e89bc
madekot <mgolubcov@gmail.com>
2019-06-16 16:52:20 +0300

    Начал писать решение задания: подробности

--- js/main.js
+++ js/main.js
@@ -144,3 +144,125 @@ var renderPhotosElements = function (simulationParrentElement, mockArr) {
 };
 
 renderPhotosElements(simulationPictureElement, photoMocks);
+
+// Решение задания: подробности;
+
+// 1.1 Открытие закрытие формы редактирования;
+var formElement = document.querySelector('.img-upload'); // форма редактирования фото
+var uploadFileFieldElement = formElement.querySelector('#upload-file');
+var imgUploadOverlayElement = formElement.querySelector('.img-upload__overlay');
+var closeButtonImageEditingFormElement = formElement.querySelector('#upload-cancel');
+
+var onСloseButtonImageEditingFormClick = function () { // не уверен в названии;
+  closeImageEditingForm();
+};
+
+var openImageEditingForm = function () { // открывает попап редактирования;
+  sizeFieldElement.value = DEFAULT_VALUE_SIZE_FIELD + '%'; // Устанавливает значение по умолчанию, для поля размера фото;
+
+  imgUploadOverlayElement.classList.remove('hidden');
+  closeButtonImageEditingFormElement.addEventListener('click', onСloseButtonImageEditingFormClick);
+  document.addEventListener('keydown', onImageEditingFormEscPress);
+  increaseButtonElement.addEventListener('click', onIncreaseButtonClick);
+  decreaseButtonElement.addEventListener('click', onDecreaseButtonClick);
+  imagePreview.style.transform = 'scale(' + DEFAULT_VALUE_SIZE_FIELD_TRANSFORM + ')'; // сбрасывает размер изображения формы редактирования к дефолтному значению;
+};
+
+var closeImageEditingForm = function () { // закрывает попап редактирования;
+  uploadFileFieldElement.value = ''; // сбрасываю значения поля. для поторной работы события 'change';
+  imgUploadOverlayElement.classList.add('hidden');
+  closeButtonImageEditingFormElement.removeEventListener('click', onСloseButtonImageEditingFormClick);
+  document.removeEventListener('keydown', onImageEditingFormEscPress);
+  increaseButtonElement.removeEventListener('click', onIncreaseButtonClick);
+  decreaseButtonElement.removeEventListener('click', onDecreaseButtonClick);
+};
+
+uploadFileFieldElement.addEventListener('change', function () { // вешает обработчик на поле загрузки фото и слушает событие change;
+  openImageEditingForm();
+});
+
+var KEY_CODE_ESC = 27;
+var onImageEditingFormEscPress = function (evt) { // не уверен в названии;
+  if (evt.keyCode === KEY_CODE_ESC) {
+    closeImageEditingForm();
+  }
+};
+
+// 1.2 Редактирование размера изображения;
+
+// 1.2.1 Изменение поля размера фото
+var DEFAULT_VALUE_SIZE_FIELD = 100;
+var DEFAULT_VALUE_SIZE_FIELD_TRANSFORM = 1;
+
+var RESIZING_STEP = 25;
+var MIN_SIZE = 25;
+var MAX_SIZE = 100;
+var NUMBER_SYSTEM = 10;
+
+var sizeFieldElement = formElement.querySelector('.scale__control--value'); // поле размера фото;
+var increaseButtonElement = formElement.querySelector('.scale__control--bigger');
+var decreaseButtonElement = formElement.querySelector('.scale__control--smaller');
+
+var increaseFieldValue = function () { // Увеличивает значения поля размера фото
+  var sizeFieldElementvalue = parseInt(sizeFieldElement.value, NUMBER_SYSTEM); // вытаскивает число из строки значения поля;
+  if (sizeFieldElementvalue < MAX_SIZE) {
+    sizeFieldElementvalue += RESIZING_STEP;
+    sizeFieldElement.value = sizeFieldElementvalue + '%';
+  }
+  return sizeFieldElementvalue;
+};
+
+var decreaseFieldValue = function () { // Уменьшает значения поля размера фото
+  var sizeFieldElementvalue = parseInt(sizeFieldElement.value, NUMBER_SYSTEM); // вытаскивает число из строки значения поля;
+  if (sizeFieldElementvalue > MIN_SIZE) {
+    sizeFieldElementvalue -= RESIZING_STEP;
+    sizeFieldElement.value = sizeFieldElementvalue + '%';
+  }
+  return sizeFieldElementvalue;
+};
+
+var onIncreaseButtonClick = function () { // Не уверен в названии, функция обработчика увеличения значения поля размера;
+  resizePhotoPreview(increaseFieldValue());
+};
+
+var onDecreaseButtonClick = function () { // Не уверен в названии, функция обработчика уменьшения значения поля размера;
+  resizePhotoPreview(decreaseFieldValue());
+};
+
+// 1.2.2  Изменение размера фото
+var imagePreview = formElement.querySelector('.img-upload__preview img');
+var resizePhotoPreview = function (value) {
+  value = value / 100;
+  imagePreview.style.transform = 'scale(' + value + ')';
+};
+
+// 1.3 Наложение эффекта на изображение:
+
+// 1.3.1 Добавляет класс изображению формы редактирования, через клик по превью картинке;
+var FILTER_DEFAULT_CLASS = 'effects__preview--original';
+var FILTER_CLASSES = [FILTER_DEFAULT_CLASS, 'effects__preview--chrome', 'effects__preview--sepia', 'effects__preview--marvin', 'effects__preview--phobos', 'effects__preview--heat'];
+var imagesFilterPreviewElements = formElement.querySelectorAll('.effects__item');
+
+var addClassAddChangeEvent = function (imageFilterPreviewElement, filterClass) {
+  imageFilterPreviewElement.addEventListener('change', function () {
+    resetClassListFilter(imagePreview, FILTER_CLASSES);
+    imagePreview.classList.add(filterClass);
+  });
+};
+
+var addClassAddChangeEvents = function (imagesPreviewFilters, filterClasses) {
+  imagesFilterPreviewElements.forEach(function (imagePreviewfilter, i) {
+    var filterClass = filterClasses[i];
+    addClassAddChangeEvent(imagePreviewfilter, filterClass);
+  });
+};
+
+var resetClassListFilter = function (element, filterClasses) {
+  filterClasses.forEach(function (nameClass) {
+    element.classList.remove(nameClass);
+  });
+};
+
+addClassAddChangeEvents(imagesFilterPreviewElements, FILTER_CLASSES);
+
+// Меняет интенсивность эффекта через перемещением ползунка в слайдереж;

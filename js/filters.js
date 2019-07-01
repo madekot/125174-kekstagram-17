8343fc4be62e9b0a2f94236afca12afb23c62bb0
madekot <mgolubcov@gmail.com>
2019-07-01 13:58:01 +0300

    Написал решение задания: перламутровые пуговицы

---
+++ js/filters.js
@@ -0,0 +1,63 @@
+'use strict';
+(function () {
+  var RANDOM_CARD = 10;
+
+  var originalDataCards = [];
+
+  var onLoad = function (data) {
+    originalDataCards = data;
+    window.picture.render(data);
+  };
+
+  window.backend.load(onLoad);
+
+  var menu = document.querySelector('.img-filters');
+  menu.classList.remove('img-filters--inactive');
+
+  var popularFilter = menu.querySelector('#filter-popular');
+
+  var removePhotoCards = function () {
+    var photoCards = document.querySelectorAll('.picture');
+    for (var i = photoCards.length - 1; i >= 0; i--) {
+      var photoCard = photoCards[i];
+      photoCard.parentElement.removeChild(photoCard);
+    }
+  };
+
+  popularFilter.addEventListener('click', function () {
+    window.debounce(function () {
+      removePhotoCards();
+      window.picture.render(originalDataCards);
+    });
+  });
+
+  var getRandomCards = function () {
+    var cloneDataCards = originalDataCards.slice();
+    return window.utility.random.shuffleArray(cloneDataCards).slice(0, RANDOM_CARD);
+  };
+
+  var newFilter = menu.querySelector('#filter-new');
+  newFilter.addEventListener('click', function () {
+    window.debounce(function () {
+      removePhotoCards();
+      window.picture.render(getRandomCards());
+    });
+  });
+
+  var getDebateCards = function () {
+    var cloneDataCards = originalDataCards.slice();
+    cloneDataCards.sort(function (a, b) {
+      return a.comments.length > b.comments.length ? 1 : -1;
+    });
+    return cloneDataCards;
+  };
+
+  var discussedFilter = menu.querySelector('#filter-discussed');
+  discussedFilter.addEventListener('click', function () {
+    window.debounce(function () {
+      removePhotoCards();
+      window.picture.render(getDebateCards());
+    });
+  });
+
+})();

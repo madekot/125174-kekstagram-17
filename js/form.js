35bad1a99eb6ab68e6665f1f2499cedc0f9675a7
madekot <mgolubcov@gmail.com>
2019-07-06 19:19:23 +0300

    Поправил отправку данных формы

--- js/form.js
+++ js/form.js
@@ -1,17 +1,11 @@
 'use strict';
 (function () {
 
-  var imageEditingFormElement = document.querySelector('.img-upload');
   var submitBatton = document.querySelector('.img-upload__submit');
 
   var checkOnFocus = function (evt, element) {
     return evt.target !== element;
   };
-
-
-  imageEditingFormElement.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-  });
 
   submitBatton.addEventListener('click', function () {
     window.validation.checks();
